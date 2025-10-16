ab62ce6a981bd79f2f98868fd7a6b6aca21796b9
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2025-10-16 16:16:49 +0000

    WIP: add ability to use cached distances

--- mllam_data_prep/ops/cropping.py
+++ mllam_data_prep/ops/cropping.py
@@ -254,22 +254,27 @@ def distance_to_convex_hull_boundary(
 
     # Calculate minimum distance to each arc and take the minimum
     # distance over all arcs
-    import ipdb; ipdb.set_trace()
-    #arcs = np.stack(chull_arcs)
-    #global pbar
-    #with tqdm(total=len(chull_arcs)) as pbar:
-    #    distances = shortest_distance_to_arc(da_xyz, arcs[:,0], arcs[:,1])
-    #mindist_to_ref = distances.min(axis=0)
+    # import ipdb; ipdb.set_trace()
+    use_cached_distances = False
+    if use_cached_distances:
+        print("Using cached distances")
+        da_mindist_to_ref = xr.open_dataset("/home/has/repos/mllam-exps-ShCu/da_mindist_to_ref.nc")['da_mindist_to_ref']
+    else:
+        print("Calculating distances")
+        arcs = np.stack(chull_arcs)
+        global pbar
+        with tqdm(total=len(chull_arcs)) as pbar:
+            distances = shortest_distance_to_arc(da_xyz, arcs[:,0], arcs[:,1])
+        mindist_to_ref = distances.min(axis=0)
 
-    #da_mindist_to_ref = xr.DataArray(
-    #    mindist_to_ref, coords=ds_exterior_lat.coords, dims=ds_exterior_lat.dims
-    #)
-    #da_mindist_to_ref.attrs[
-    #    "long_name"
-    #] = "minimum distance to convex hull boundary of reference dataset"
-    #da_mindist_to_ref.attrs["units"] = "radians"
-
-    da_mindist_to_ref = xr.open_dataset("/home/has/repos/mllam-exps-ShCu/da_mindist_to_ref.nc")['da_mindist_to_ref']
+        da_mindist_to_ref = xr.DataArray(
+            mindist_to_ref, coords=ds_exterior_lat.coords, dims=ds_exterior_lat.dims
+        )
+        da_mindist_to_ref.attrs[
+            "long_name"
+        ] = "minimum distance to convex hull boundary of reference dataset"
+        da_mindist_to_ref.attrs["units"] = "radians"
+        # da_mindist_to_ref.to_netcdf("/home/has/repos/mllam-exps-ShCu/da_mindist_to_ref.nc")
 
     if include_convex_hull_mask:
         return da_mindist_to_ref, da_ch_mask
